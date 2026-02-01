8809de89df16cac4ef8c02092b1ad7ef838cfe6b
Shayan Riyaz <Shayanriyaz@icloud.com>
2026-02-01 11:45:38 -0800

    moved images

--- src/data/site.ts
+++ src/data/site.ts
@@ -4,7 +4,7 @@ export const site = {
   url: "https://shayanriyaz.github.io",
   owner: {
     name: "Shayan Riyaz",
-    avatar: "Shayan.png",
+    avatar: "images/Shayan.png",
     bio: "Software Research and Development Engineer with half a decade of experience in the biomechanics and biomedical industry.",
     email: "shayanariyaz@gmail.com",
     twitter: "shayanriyaz",
